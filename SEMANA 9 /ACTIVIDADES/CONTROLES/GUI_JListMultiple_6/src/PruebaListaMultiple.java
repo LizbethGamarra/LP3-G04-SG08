b32343196c2f31b2423b46f724d2f583d0a0a987
MathiasOstolaza <mathiasysaacostolazaalvarez@gmail.com>
2025-11-16 19:40:15 -0500

    Add files via upload

---
+++ SEMANA 9 /ACTIVIDADES/CONTROLES/GUI_JListMultiple_6/src/PruebaListaMultiple.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class PruebaListaMultiple {
+    public static void main(String[] args) {
+
+        MarcoListaMultiple ventana = new MarcoListaMultiple();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(350, 250);
+        ventana.setVisible(true);
+    }
+}
